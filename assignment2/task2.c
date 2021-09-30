18de9d01e11cffadc9438cf7862c508dd38ea819
Legolass322 <l.e.g.o.l.a.s.s.dm@gmail.com>
2021-09-30 20:12:22 +0300

    Update task2.c

--- assignment2/task2.c
+++ assignment2/task2.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 typedef char Name[31];
 typedef struct Date Date;
 typedef struct Item Item;
 typedef struct Resort Resort;
+
+int check_name_char(char);
+int check_date(Date*);
+int leap_year(int);
+
+int read_name(FILE*, char*);
+int read_date(FILE*, Date*);
+int read_item(FILE*, Item*);
+
+
+int main() {
+    FILE* fp = fopen("input.txt", "r");
+
+    int flag = 1;
+    Name name;
+    Date date;
+    // reading data
+    flag *= read_name(fp, name);
+    flag *= read_date(fp, &date);
+
+    fclose(fp);
+
+    return 0;
+}
 
 struct Date {
     int day;
@@ -16,16 +41,51 @@ struct Date {
     int seconds;
 };
 
-int check_name_char(char);
-int check_date(Date);
-int ckeck_item(Item);
+struct Item {
+    char name[16];
+    float size;
+    int amount;
+    char unit[6];
+};
 
-int read_name(FILE*, char*);
-
+int leap_year(int y) {
+    if (y % 4 == 0) {
+        if (y % 100 == 0) {
+            return y % 400 == 0;
+        }
+        return 1;
+    }
+    return 0;
+}
 
 int check_name_char(char c) {
-    if (!(((int) c >= (int) 'a' && (int) c <= (int) 'z') || ((int) c >= (int) 'A' && (int) c <= (int) 'Z') || c == ' '))
-        return 0;
+    return (   ((int) c >= (int) 'a' && (int) c <= (int) 'z') 
+            || ((int) c >= (int) 'A' && (int) c <= (int) 'Z') 
+            || c == ' ');
+}
+
+int check_date(Date* date) {
+    int max_day;
+
+    if (date->year < 1) return 0;
+    if (date->month < 1 || date->month > 12) return 0;
+
+    if (date->month == 1 || date->month == 3
+            || date->month == 5 || date->month == 7
+            || date->month == 8 || date->month == 10
+            || date->month == 12) {
+        max_day = 31;
+    } else if (date->month == 2) {
+        if (leap_year(date->year)) max_day = 29;
+        else max_day = 28;
+    } else max_day = 30;
+
+    if (date->day < 1 || date->day > max_day) return 0;
+
+    if(date->hours > 23 || date->hours < 0) return 0;
+    if(date->minits > 59 || date->minits < 0) return 0;
+    if(date->seconds > 59 || date->seconds < 0) return 0;
+
     return 1;
 }
 
@@ -44,13 +104,9 @@ int read_name(FILE* fp, char* name) {
     return 1;
 }
 
-int main() {
-    FILE* fp = fopen("input.txt", "r");
-
-    int flag = 1;
-    // reading name
-    Name name;
-    flag *= read_name(fp, name);
-    
-    return 0;
+int read_date(FILE* fp, Date* date) {
+    fscanf(fp, "%d/%d/%d %d:%d:%d", &date->day, 
+    &date->month, &date->year, &date->hours, &date->minits,
+    &date->seconds);
+    return check_date(date);
 }
