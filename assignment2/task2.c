baa65603deb6b644156ebd1631df25b9d3612bf8
Legolass322 <l.e.g.o.l.a.s.s.dm@gmail.com>
2021-10-03 19:12:12 +0300

    reading name&date

--- assignment2/task2.c
+++ assignment2/task2.c
@@ -1,35 +1,22 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 
 typedef char Name[31];
 typedef struct Date Date;
 typedef struct Item Item;
 typedef struct Resort Resort;
 
+int is_leap_year(int);
+int is_digit(char);
+int is_alpha(char);
+
 int check_name_char(char);
 int check_date(Date*);
-int leap_year(int);
 
-int read_name(FILE*, char*);
-int read_date(FILE*, Date*);
-int read_item(FILE*, Item*);
+int read_name(char*, char*);
+int read_date(char*, Date*);
+int read_item(char*, Item*);
 
-
-int main() {
-    FILE* fp = fopen("input.txt", "r");
-
-    int flag = 1;
-    Name name;
-    Date date;
-    // reading data
-    flag *= read_name(fp, name);
-    flag *= read_date(fp, &date);
-
-    fclose(fp);
-
-    return 0;
-}
 
 struct Date {
     int day;
@@ -39,16 +26,13 @@ struct Date {
     int hours;
     int minits;
     int seconds;
+
+    char str_date[11];
+    char str_time[9];
 };
 
-struct Item {
-    char name[16];
-    float size;
-    int amount;
-    char unit[6];
-};
 
-int leap_year(int y) {
+int is_leap_year(int y) {
     if (y % 4 == 0) {
         if (y % 100 == 0) {
             return y % 400 == 0;
@@ -58,10 +42,17 @@ int leap_year(int y) {
     return 0;
 }
 
-int check_name_char(char c) {
+int is_digit(char c) {
+    return (int) c >= (int) '0' && (int) c <= '9';
+}
+
+int is_alpha(char c) {
     return (   ((int) c >= (int) 'a' && (int) c <= (int) 'z') 
-            || ((int) c >= (int) 'A' && (int) c <= (int) 'Z') 
-            || c == ' ');
+            || ((int) c >= (int) 'A' && (int) c <= (int) 'Z'));
+}
+
+int check_name_char(char c) {
+    return (is_alpha(c) || c == ' ');
 }
 
 int check_date(Date* date) {
@@ -76,7 +67,7 @@ int check_date(Date* date) {
             || date->month == 12) {
         max_day = 31;
     } else if (date->month == 2) {
-        if (leap_year(date->year)) max_day = 29;
+        if (is_leap_year(date->year)) max_day = 29;
         else max_day = 28;
     } else max_day = 30;
 
@@ -89,24 +80,71 @@ int check_date(Date* date) {
     return 1;
 }
 
-int read_name(FILE* fp, char* name) {
-    char c = 0;
-    int length;
-    while (c != '\n' && length <= 30) {
-        c = fgetc(fp);
-        if (!check_name_char(c)) return 0;
-        *(name + length) = c;
-        length += 1;
-        if (length > 30) {
-            return 0;
-        }
-    }
+int read_name(char* buffer, char* name) {
+    /* name : [2;30] */
+    int len = strlen(buffer);
+    if (len < 2 || len > 30) return 0;
+
+    char *tmpb = buffer;
+    char *tmpn = name;
+    do {
+        if (!(check_name_char(*(tmpb)) || *(tmpb) == '\0')) return 0;
+        else *(tmpn++) = *(tmpb++);
+    } while(*tmpb != '\0');
+
     return 1;
 }
 
-int read_date(FILE* fp, Date* date) {
-    fscanf(fp, "%d/%d/%d %d:%d:%d", &date->day, 
-    &date->month, &date->year, &date->hours, &date->minits,
-    &date->seconds);
+int read_date(char* buffer, Date* date) {
+    // checking format
+    if (strlen(buffer) != 19) return 0;
+
+    for(int i = 0; i < 19; i++) {
+        if (i == 2 || i == 5)
+            if (*(buffer + i) != '/') return 0;
+        else if (i == 13 || i == 16)
+            if (*(buffer + i) != ':') return 0;
+        else 
+            if (!is_digit(*(buffer + i)) || *(buffer + i) != ' ') return 0;
+    }
+
+    // reading str values
+    for (int i = 0; i < 10; i++) {
+        date->str_date[i] = *(buffer + i);
+    }
+    date->str_date[10] = '\0';
+    for (int i = 11; i < 19; i++) {
+        date->str_time[i - 11] = *(buffer + i);
+    }
+    date->str_time[8] = '\0';
+
+    // reading int values
+    sscanf(buffer, "%d/%d/%d %d:%d:%d", &((*date).day), &((*date).month),
+        &((*date).year), &((*date).hours), &((*date).minits), &((*date).seconds));
+
+    // checking values
     return check_date(date);
+}
+
+int main() {
+    char buffer[100];
+    int flag = 1;
+
+    Name name;
+    Date date;
+
+    FILE* fp = fopen("input.txt", "r");
+
+    fscanf(fp, "%[^\n]\n", buffer);
+    flag *= read_name(buffer, name);
+    if (!flag) printf("Invalid1");
+    fscanf(fp, "%[^\n]\n", buffer);
+    flag *= read_date(buffer, &date);
+    if (!flag) printf("Invalid1");;
+
+    fclose(fp);
+
+    printf("%s %s %d", date.str_date, date.str_time, flag);
+
+    return 0;
 }
