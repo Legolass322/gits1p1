0270a988663291505d2e27ca590d61e97a3b30d7
Legolass322 <l.e.g.o.l.a.s.s.dm@gmail.com>
2021-10-03 21:52:37 +0300

    Update task2.c

--- assignment2/task2.c
+++ assignment2/task2.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
 
 typedef char Name[31];
 typedef struct Date Date;
 typedef struct Item Item;
-typedef struct Resort Resort;
+typedef struct Row Row;
 
 int is_leap_year(int);
 int is_digit(char);
@@ -12,6 +14,7 @@ int is_alpha(char);
 
 int check_name_char(char);
 int check_date(Date*);
+int check_size(char*);
 
 int read_name(char*, char*);
 int read_date(char*, Date*);
@@ -31,6 +34,19 @@ struct Date {
     char str_time[9];
 };
 
+struct Item {
+    char name[16];
+    char size[16];
+    int amount;
+    char unit[6];
+};
+
+struct Row {
+    int count_item;
+    Name name;
+    Date date;
+    Item items[10];
+};
 
 int is_leap_year(int y) {
     if (y % 4 == 0) {
@@ -80,6 +96,18 @@ int check_date(Date* date) {
     return 1;
 }
 
+int check_size(char* buf) {
+    /* float (0; 200] */
+    for (int i = 0; i < strlen(buf); i++) {
+        if (!is_digit(*(buf + i)) && *(buf + i) != '.') return 0; // fix: 16.16.16
+    }
+    char* end;
+    float f = strtof(buf, &end);
+    if (f <= 0 ||  f > 200) return 0;
+
+    return 1;
+}
+
 int read_name(char* buffer, char* name) {
     /* name : [2;30] */
     int len = strlen(buffer);
@@ -105,7 +133,7 @@ int read_date(char* buffer, Date* date) {
         else if (i == 13 || i == 16)
             if (*(buffer + i) != ':') return 0;
         else 
-            if (!is_digit(*(buffer + i)) || *(buffer + i) != ' ') return 0;
+            if (!(is_digit(*(buffer + i)) || *(buffer + i) == ' ')) return 0;
     }
 
     // reading str values
@@ -126,25 +154,140 @@ int read_date(char* buffer, Date* date) {
     return check_date(date);
 }
 
+int read_item(char* buffer, Item* item) {
+    int length = strlen(buffer);
+    char tmp_buf[32];
+    char *pend;
+    // backward reading
+    int i = length;
+    int end = length;
+
+    while (*(buffer + i) == ' ' && i >= 0) i--, end--; // useless spaces
+    while (*(buffer + i) != ' ' && i >= 0) i--; // units
+    for (int j = i + 1; j < end + 1; j++) {
+        tmp_buf[j - i - 1] = *(buffer + j);
+    }
+    tmp_buf[end - i] = '\0';
+    end = i;
+    if (!(strcmp(tmp_buf, "pcs\0") == 0 || strcmp(tmp_buf, "pair\0") == 0)) return 0;
+    strcpy(item->unit, tmp_buf);
+
+    while (*(buffer + i) == ' ' && i >= 0) i--, end--; // useless spaces
+    while (*(buffer + i) != ' ' && i >= 0) i--; // amount
+    for (int j = i + 1; j < end + 1; j++) {
+        tmp_buf[j - i - 1] = *(buffer + j);
+    }
+    tmp_buf[end - i] = '\0';
+    end = i;
+    item->amount = strtol(tmp_buf, &pend, 10);
+    if (item->amount > 1 && strcmp(item->unit, "pair\0") == 0) {
+        item->unit[4] = 's';
+        item->unit[5] = '\0';
+    }
+
+    while (*(buffer + i) == ' ' && i >= 0) i--, end--; // useless spaces
+    while (*(buffer + i) != ' ' && i >= 0) i--; // size
+    for (int j = i + 1; j < end + 1; j++) {
+        item->size[j - i - 1] = *(buffer + j);
+    }
+    item->size[end - i] = '\0';
+    end = i;
+    if (!check_size(item->size)) return 0;
+
+    while (*(buffer + i) == ' ' && i >= 0) i--, end--; // useless spaces
+    i = 0; // name
+    if (end + 1 < 4 || end + 1 > 15) return 0;
+    for (int j = i; j < end + 1; j++) {
+        if (!check_name_char(*(buffer + j))) return 0;
+        (*item).name[j - i] = *(buffer + j);
+    }
+    return 1;
+}
+
 int main() {
     char buffer[100];
     int flag = 1;
 
-    Name name;
-    Date date;
+    int count = 0;
+    Row resort[30];
 
     FILE* fp = fopen("input.txt", "r");
+    FILE* op = fopen("output.txt", "w");
 
-    fscanf(fp, "%[^\n]\n", buffer);
-    flag *= read_name(buffer, name);
-    if (!flag) printf("Invalid1");
-    fscanf(fp, "%[^\n]\n", buffer);
-    flag *= read_date(buffer, &date);
-    if (!flag) printf("Invalid1");;
+    // reading
+    char c = 0;
+    while (c != EOF) {
+        // name
+        fscanf(fp, "%[^\n]\n", buffer);
+        flag *= read_name(buffer, resort[count].name);
+        if (!flag) {
+            fprintf(op, "Invalid input!");
+            return 0;
+        }
+        // date&time
+        fscanf(fp, "%[^\n]\n", buffer);
+        flag *= read_date(buffer, &(resort[count].date));
+        if (!flag) {
+            fprintf(op, "Invalid input!");
+            return 0;
+        }
+        // items
+        int is_next = 0;
+        int i = 0;
+        resort[count].count_item = 0;
+        do {
+            i = 0;
+            c = fgetc(fp);
+            while (c != '\n' && c != EOF) {
+                buffer[i] = c;
+                i++;
+                c = fgetc(fp);
+            }
+            buffer[i] = '\0';
+            printf("%s %d %d \n", buffer, count, resort[count].count_item);
+            if (strlen(buffer) == 0) {
+                if (resort[count].count_item == 0) {
+                    fprintf(op, "Invalid input!");
+                    return 0;
+                }
+                is_next = 1;
+            } else {
+                flag *= read_item(buffer, &(resort[count].items[resort[count].count_item]));
+                if (!flag) {
+                    fprintf(op, "Invalid input!");
+                    return 0;
+                }
+                resort[count].count_item++;
+            }
+        } while (c != EOF && !is_next);
+        count++;
+    }
+
+    // writting
+    for (int i = 0; i < count; i++) {
+        fprintf(op, "%s ", resort[i].name);
+        fprintf(op, "has rented ");
+        for (int j = 0; j < resort[i].count_item; j++) {
+            fprintf(op, "%d ", resort[i].items[j].amount);
+            fprintf(op, "%s ", resort[i].items[j].unit);
+            fprintf(op, "of ");
+            fprintf(op, "%s ", resort[i].items[j].name);
+            fprintf(op, "of size ");
+            fprintf(op, "%s", resort[i].items[j].size);
+            if (j != resort[i].count_item - 1) {
+                if (resort[i].count_item - j - 2 == 0)
+                    fprintf(op, " and ");
+                else fprintf(op, ", ");
+            }
+        }
+        fprintf(op, " on ");
+        fprintf(op, "%s ", resort[i].date.str_date);
+        fprintf(op, "at ");
+        fprintf(op, "%s", resort[i].date.str_time);
+        if (i != count - 1) fprintf(op, "\n");
+    }
 
     fclose(fp);
-
-    printf("%s %s %d", date.str_date, date.str_time, flag);
-
+    fclose(op);
     return 0;
 }
